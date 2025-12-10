801d9251efed12e8f79e1ed5b3dcbf727022c977
qianjunfan <qianjunfan0@gmail.com>
2025-12-11 02:39:33 +0800

    Dec 11, 2025

---
+++ .push.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+gcc -D_GNU_SOURCE alfred.c -o alfred
+./alfred
+
+cat log > README
+rm -f log alfred a.out
+
+COMMIT_MESSAGE=$(date +"%b %d, %Y")
+
+git add .
+git commit -m "$COMMIT_MESSAGE"
+
+git push
--- push.sh
+++
@@ -1,8 +0,0 @@
-gcc -D_GNU_SOURCE alfred.c -o alfred 
-./alfred 
-cat log > README
-rm -f log alfred a.out
-cat README
-git add .
-git commit -m "new"
-git push
