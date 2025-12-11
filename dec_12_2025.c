029921b78fa43e5402214263392151fddc9c6950
Qian Junfan <qianjunfan0@gmail.com>
2025-12-12 00:38:25 +0900

    Dec 12, 2025

--- dec_12_2025.c
+++ dec_12_2025.c
@@ -21,3 +21,37 @@ int lengthOfLastWord(char *s)
 	}
 	return 0;
 }
+
+
+/* lc0067 - add binary */
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+char *addBinary(char *a, char *b)
+{
+	int l_a = strlen(a);
+	int l_b = strlen(b);
+	int l_r = (l_a > l_b ? l_a : l_b) + 1;
+	char *r = (char *)malloc((l_r + 1) * sizeof(char));
+
+	int i = l_a - 1;
+	int j = l_b - 1;
+	int k = l_r;
+	r[k--] = '\0';
+	int c = 0;
+	while (i >= 0 || j >= 0 || c) {
+		int sum = c;
+		if (i >= 0) sum += a[i--] - '0';
+		if (j >= 0) sum += b[j--] - '0';
+		if (k < 0)
+			break;
+		r[k--] = (sum % 2) + '0';
+		c = sum / 2;
+	}
+
+	if (k == -1) 
+		return r;
+	else 
+		return r + 1;
+}
